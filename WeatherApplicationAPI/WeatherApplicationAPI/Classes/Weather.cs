73f6afc20a9a9770a47a3e1651af0b967bea4b9d
TeemuHe <teemuhoo@luukku.com>
2022-12-06 18:20:21 +0200

    repository created

---
+++ WeatherApplicationAPI/WeatherApplicationAPI/Classes/Weather.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApplicationAPI.Classes
+{
+    public class Weather
+    {
+    }
+}
